a417c0d9b96b5b41ab75260461b18c7c8731d3d3
ROMAN <poman207575@gmail.com>
2023-03-02 02:19:25 +0200

    dev

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,4 @@
-let number_one_element =  document.querySelector('.number_one'),
-    number_two_element = document.querySelector('.number_two'),
-    number_three_element = document.querySelector('.number_three'),
+let elementsNum = document.querySelectorAll('.characteristic__number'),
     header = document.querySelector('.header'),
     body = document.querySelector('body'),
     headElement = document.querySelector('.head'),
@@ -27,52 +25,8 @@ let number_one_element =  document.querySelector('.number_one'),
         }else{}
     }
 
-    function counter(number, element, second){
-                let num = 0;
-                if((number % 1) == 0){
-                    second = (second * 1000) / (number * 2);
-                }else{
-                    second = (second * 1000) / (number * 2 * 10);
-                }
-                function get(){
-                    if((number % 1) == 0){
 
-                        if(num < number && num < (number / 100 * 80)){
-                            num++;
-                            element.innerHTML = num;
-                            setTimeout(get, second / 2);
-                        }else if(num < number && num < (number / 100 * 90)){
-                            num++;
-                            element.innerHTML = num;
-                            setTimeout(get, second * 2);
-                        }else if(num < number){
-                            num++;
-                            element.innerHTML = num;
-                            setTimeout(get, second * 4);
-                        }
-
-                    }else{
-                        let numberTwo = number * 10;
-                        if(num < numberTwo && num < (numberTwo / 100 * 80)){
-                            num++;
-                            element.innerHTML = num / 10;
-                            setTimeout(get, second / 2);
-                        }else if(num < numberTwo && num < (numberTwo / 100 * 90)){
-                            num++;
-                            element.innerHTML = num / 10;
-                            setTimeout(get, second * 2);
-                        }else if(num < numberTwo){
-                            num++;
-                            element.innerHTML = num / 10;
-                            setTimeout(get, second * 4);
-                        }
-
-                    }
-                }
-                    setTimeout(get, second);
-            }
-    let check = 0,
-        checkTwo = 0;
+    let check = 0;
 
 document.addEventListener("scroll", (e) => {
     var el = document.querySelector(".characteristic");
@@ -85,26 +39,25 @@ document.addEventListener("scroll", (e) => {
         specsElement.style.backgroundPositionY = `${elementInViewportTwo(specsElement) /2.5 + 40}%`
     }
 
-    if(inViewport && check == 0){
-        counter(405, number_one_element, 1);
-        counter(7, number_two_element, 7);
-        counter(3.5, number_three_element, 5);
-        check++;
-    }
     if(window.innerWidth <= 692){
-        if(Math.floor(window.scrollY) >= 150 && checkTwo == 0){
+        if(Math.floor(window.scrollY) >= 150 && check == 0){
             header.style.backgroundColor = '#15181F';
-            checkTwo++;
-        }else if (Math.floor(window.scrollY) <= 150 && checkTwo == 1){
-            checkTwo--;
+            check++;
+        }else if (Math.floor(window.scrollY) <= 150 && check == 1){
+            check--;
             header.style.backgroundColor = '';
         }
+    }
+    console.log(elementInViewport(elementsNum[0]));
+    if(elementInViewport(elementsNum[0])){
+        elementsNum[0].style.cssText = 'counter-reset: ms var(--numberOne); animation: count 4s;';
+        elementsNum[1].style.cssText = 'counter-reset: ms var(--numberTwo); animation: count 4s;';
+        elementsNum[2].style.cssText = 'counter-reset: ms var(--numberThree); animation: count 4s;';
     }
     
 });
 
 document.querySelector('.header__button-burger').addEventListener('click', (event) => {
     event.target.closest('.header').classList.toggle('active');
-    console.log(event.target.closest('.header'));
     body.classList.toggle('disable');
 });
